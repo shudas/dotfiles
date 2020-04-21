e943cab386e57bf9b9f0c93a59fc3ed0348f4a48
Shuvajit Das <shudas@umich.edu>
2020-04-21 01:55:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t shudas/dotfiles:latest -f Dockerfile .
 bash install.sh uninstall_all
 bash install.sh install_bash
 bash install.sh install_git
 bash install.sh install_screen
+docker build -t shudas/dotfiles:latest -f Dockerfile .
